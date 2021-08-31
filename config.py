e9125a7f7aa2b3c4c633ccc7e0cd8e2ac90b3115
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-08-31 16:18:36 +0200

    [add] safe rating [add] help and version

--- config.py
+++ config.py
@@ -17,11 +17,19 @@ RANDOM_DICTIONARY = ["anime", "japan", "manga",
 REQUEST_CACHE_TTL = 3600
 GIPHY_SEARCH_ENDPOINT = "http://api.giphy.com/v1/gifs/search?q={query}&api_key=" + \
     GIPHY_API_KEY + "&limit=100"
-GIPHY_RANDOM_ENDPOINT = "https://api.giphy.com/v1/gifs/random?api_key=" + GIPHY_API_KEY
+GIPHY_RANDOM_ENDPOINT = "https://api.giphy.com/v1/gifs/random?&api_key=" + GIPHY_API_KEY
 TENOR_SEARCH_ENDPOINT = "https://g.tenor.com/v1/search?q={query}&key=" + \
     TENOR_API_KEY + "&limit=50"
 TENOR_RANDOM_ENDPOINT = "https://g.tenor.com/v1/random?q={query}&key=" + \
     TENOR_API_KEY + "&limit=50"
+
+SFW_GIPHY_SEARCH_ENDPOINT = "http://api.giphy.com/v1/gifs/search?q={query}&api_key=" + \
+    GIPHY_API_KEY + "&limit=100&rating=pg13"
+SFW_GIPHY_RANDOM_ENDPOINT = "https://api.giphy.com/v1/gifs/random?rating=pg13&api_key=" + GIPHY_API_KEY
+SFW_TENOR_SEARCH_ENDPOINT = "https://g.tenor.com/v1/search?q={query}&key=" + \
+    TENOR_API_KEY + "&limit=50&contentfilter=low"
+SFW_TENOR_RANDOM_ENDPOINT = "https://g.tenor.com/v1/random?q={query}&key=" + \
+    TENOR_API_KEY + "&limit=50&contentfilter=low"
 
 # Assets
 ASSETS_DOMAIN = "https://easygif-assets.netlify.app"
---
+++ run.py
@@ -0,0 +1,29 @@
+import sys
+from config import EASYGIF_VERSION, DEBUG_MODE
+
+if "-v" in sys.argv or "--version" in sys.argv:
+    print(f"EasyGif {EASYGIF_VERSION}")
+    quit()
+
+if "-h" in sys.argv or "--help" in sys.argv:
+    print()
+    print("                EASYGIF SERVER HELP CENTER")
+    print()
+    print(f"EasyGif {EASYGIF_VERSION}")
+    print(f"DEBUG_MODE: {DEBUG_MODE}")
+    print("""
+The main server for EasyGif.
+
+Args:
+    --clear-log                     Clears the 'easygif.log' file
+    -h, --help                      Shows the EasyGif Server Help Center and exits
+    -d, --debug                     Launches EasyGif Server in DEBUG_MODE (note: --debug enables a higher debug level)
+    -v, --version                   Shows the Server version and exits
+""")
+    quit()
+
+
+from easygif import client
+from __protected import DISCORD_BOT_TOKEN
+
+client.run(DISCORD_BOT_TOKEN)
